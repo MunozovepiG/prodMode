6b60ca7249dbf83205719bf6dfd576b5b1bda647
Muno Gwata <munozovepi@astute.money>
2023-06-25 17:50:53 +0200

    added the latest good code need find a good package for the charts

--- lib/screens/manageSavings/testStructure.dart
+++ lib/screens/manageSavings/testStructure.dart
@@ -14,143 +14,150 @@ class Test extends StatelessWidget {
         title: Text('Grouped Payments'),
       ),
       body: Center(
-        child: Column(
-          children: [
-            TextButton(
-                onPressed: () {
-                  saveDataToFirebase();
-                },
-                child: Text('test')),
-            Container(
-              height: 450,
-              child: StreamBuilder<QuerySnapshot>(
-                stream: FirebaseFirestore.instance
-                    .collection('users')
-                    .doc(FirebaseAuth.instance.currentUser?.uid)
-                    .collection('testData')
-                    .snapshots(),
-                builder: (context, snapshot) {
-                  if (snapshot.hasError) {
-                    return Text('Error: ${snapshot.error}');
-                  }
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              TextButton(
+                  onPressed: () {
+                    saveDataToFirebase();
+                  },
+                  child: Text('test')),
+              Container(
+                height: 450,
+                child: StreamBuilder<QuerySnapshot>(
+                  stream: FirebaseFirestore.instance
+                      .collection('users')
+                      .doc(FirebaseAuth.instance.currentUser?.uid)
+                      .collection('testData')
+                      .snapshots(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasError) {
+                      return Text('Error: ${snapshot.error}');
+                    }
 
-                  if (!snapshot.hasData) {
-                    return Text('No data available');
-                  }
+                    if (!snapshot.hasData) {
+                      return Text('No data available');
+                    }
 
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return CircularProgressIndicator();
-                  }
-                  List<DocumentSnapshot> documents = snapshot.data!.docs;
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return CircularProgressIndicator();
+                    }
+                    List<DocumentSnapshot> documents = snapshot.data!.docs;
 
-                  return ListView.builder(
-                    itemCount: documents.length,
-                    itemBuilder: (context, index) {
-                      Map<String, dynamic> data =
-                          documents[index].data() as Map<String, dynamic>;
-                      String goal = data?['goal'];
-                      List<dynamic> payments = [];
+                    return ListView.builder(
+                      itemCount: documents.length,
+                      itemBuilder: (context, index) {
+                        Map<String, dynamic> data =
+                            documents[index].data() as Map<String, dynamic>;
+                        String goal = data?['goal'];
+                        List<dynamic> payments = [];
 
-                      if (data?['payments'] is List<dynamic>) {
-                        payments = List.from(data?['payments']);
-                      }
+                        if (data?['payments'] is List<dynamic>) {
+                          payments = List.from(data?['payments']);
+                        }
 
-                      return InkWell(
-                        child: Card(
-                          child: ListTile(
-                            title: Text('Goal: $goal'),
-                            subtitle: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: payments.map<Widget>((payment) {
-                                int amount = payment['amount'];
-                                String date = payment['date'];
-                                return Text(
-                                    'Payment - Amount: $amount, Date: $date');
-                              }).toList(),
-                            ),
-                            trailing: ElevatedButton(
-                              onPressed: () {
-                                // Add the new payment to the payments list
-                                payments.add({
-                                  'amount': 900,
-                                  'date': DateTime.now().toString(),
-                                });
+                        return InkWell(
+                          child: Card(
+                            child: ListTile(
+                              title: Text('Goal: $goal'),
+                              subtitle: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: payments.map<Widget>((payment) {
+                                  int amount = payment['amount'];
+                                  String date = payment['date'];
+                                  return Text(
+                                      'Payment - Amount: $amount, Date: $date');
+                                }).toList(),
+                              ),
+                              trailing: ElevatedButton(
+                                onPressed: () {
+                                  // Add the new payment to the payments list
+                                  payments.add({
+                                    'amount': 900,
+                                    'date': DateTime.now().toString(),
+                                  });
 
-                                // Update the payments field in Firestore
-                                FirebaseFirestore.instance
-                                    .collection('users')
-                                    .doc(FirebaseAuth.instance.currentUser?.uid)
-                                    .collection('testData')
-                                    .doc(documents[index].id)
-                                    .update({'payments': payments})
-                                    .then((_) =>
-                                        print('Payment added successfully'))
-                                    .catchError((error) =>
-                                        print('Failed to add payment: $error'));
-                              },
-                              child: Text('Add Payment'),
-                            ),
-                          ),
-                        ),
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => EditPaymentScreen(
-                                paymentRef: FirebaseFirestore.instance
-                                    .collection('users')
-                                    .doc(FirebaseAuth.instance.currentUser?.uid)
-                                    .collection('testData')
-                                    .doc(documents[index].id),
-                                paymentIndex: index,
+                                  // Update the payments field in Firestore
+                                  FirebaseFirestore.instance
+                                      .collection('users')
+                                      .doc(FirebaseAuth
+                                          .instance.currentUser?.uid)
+                                      .collection('testData')
+                                      .doc(documents[index].id)
+                                      .update({'payments': payments})
+                                      .then((_) =>
+                                          print('Payment added successfully'))
+                                      .catchError((error) => print(
+                                          'Failed to add payment: $error'));
+                                },
+                                child: Text('Add Payment'),
                               ),
                             ),
-                          );
-                        },
-                      );
-                    },
-                  );
-                },
+                          ),
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => EditPaymentScreen(
+                                  paymentRef: FirebaseFirestore.instance
+                                      .collection('users')
+                                      .doc(FirebaseAuth
+                                          .instance.currentUser?.uid)
+                                      .collection('testData')
+                                      .doc(documents[index].id),
+                                  paymentIndex: index,
+                                ),
+                              ),
+                            );
+                          },
+                        );
+                      },
+                    );
+                  },
+                ),
               ),
-            ),
-            SizedBox(
-              height: 55,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                FirebaseFirestore.instance
-                    .collection('users')
-                    .doc(FirebaseAuth.instance.currentUser?.uid)
-                    .collection('testData')
-                    .get()
-                    .then((querySnapshot) {
-                  List<Map<String, dynamic>> data = [];
-                  for (var documentSnapshot in querySnapshot.docs) {
-                    Map<String, dynamic>? documentData =
-                        documentSnapshot.data() as Map<String, dynamic>?;
-                    if (documentData != null) {
-                      data.add(documentData);
-                    }
-                  }
-                  groupPaymentsByMonth(data);
-                }).catchError((error) {
-                  print('Error fetching data: $error');
-                });
-              },
-              child: Text('Group Payments'),
-            ),
-            TextButton(
+              SizedBox(
+                height: 55,
+              ),
+              ElevatedButton(
                 onPressed: () {
-                  readTestData();
+                  FirebaseFirestore.instance
+                      .collection('users')
+                      .doc(FirebaseAuth.instance.currentUser?.uid)
+                      .collection('testData')
+                      .get()
+                      .then((querySnapshot) {
+                    List<Map<String, dynamic>> data = [];
+                    for (var documentSnapshot in querySnapshot.docs) {
+                      Map<String, dynamic>? documentData =
+                          documentSnapshot.data() as Map<String, dynamic>?;
+                      if (documentData != null) {
+                        data.add(documentData);
+                      }
+                    }
+                    groupPaymentsByMonth(data);
+                  }).catchError((error) {
+                    print('Error fetching data: $error');
+                  });
                 },
-                child: Text('date'))
-          ],
+                child: Text('Group Payments'),
+              ),
+              TextButton(
+                  onPressed: () {
+                    readTestData();
+                  },
+                  child: Text('date'))
+            ],
+          ),
         ),
       ),
     );
   }
 
-  void groupPaymentsByMonth(List<Map<String, dynamic>> data) {
+  //grouping data
+
+  List<Map<String, dynamic>> groupPaymentsByMonth(
+      List<Map<String, dynamic>> data) {
     Map<String, num> monthlyTotals = {};
 
     DateFormat dateFormatter = DateFormat('yyyy-MM-dd HH:mm:ss.SSSSSS');
@@ -181,60 +188,74 @@ class Test extends StatelessWidget {
       final total = monthlyTotals[monthName] ?? 0;
       print('Month: $monthName, Total Amount: $total');
     }
-  }
 
-  void saveDataToFirebase() async {
-    CollectionReference ref = FirebaseFirestore.instance
-        .collection('users')
-        .doc(FirebaseAuth.instance.currentUser?.uid)
-        .collection('testData');
-
-    Map<String, dynamic> data = {
-      'goal': 'Goal 1',
-      'payments': [
-        {'amount': 500, 'date': 'June 8, 2023'},
-        {'amount': 900, 'date': 'June 19, 2023'},
-      ],
-    };
-
-    try {
-      await ref.add(data);
-      print('Data saved successfully!');
-    } catch (e) {
-      print('Error saving data: $e');
+    // Prepare data for plotting
+    List<Map<String, dynamic>> chartData = [];
+    for (int month = 1; month <= 12; month++) {
+      final monthName = DateFormat.MMMM().format(DateTime(2000, month));
+      final total = monthlyTotals[monthName] ?? 0;
+      chartData.add({
+        'month': monthName,
+        'total': total,
+      });
     }
+
+    // Return the chart data
+    return chartData;
   }
+}
 
-  void readDataFromFirebase() async {
-    CollectionReference ref = FirebaseFirestore.instance
-        .collection('users')
-        .doc(FirebaseAuth.instance.currentUser?.uid)
-        .collection('testData');
+void saveDataToFirebase() async {
+  CollectionReference ref = FirebaseFirestore.instance
+      .collection('users')
+      .doc(FirebaseAuth.instance.currentUser?.uid)
+      .collection('testData');
 
-    try {
-      QuerySnapshot querySnapshot = await ref.get();
-      if (querySnapshot.docs.isNotEmpty) {
-        for (QueryDocumentSnapshot documentSnapshot in querySnapshot.docs) {
-          Map<String, dynamic>? data =
-              documentSnapshot.data() as Map<String, dynamic>?;
-          // Access the fields of the document
-          String goal = data?['goal'];
-          List<dynamic> payments = data?['payments'];
+  Map<String, dynamic> data = {
+    'goal': 'Goal 1',
+    'payments': [
+      {'amount': 500, 'date': 'June 8, 2023'},
+      {'amount': 900, 'date': 'June 19, 2023'},
+    ],
+  };
 
-          // Print the data
-          print('Goal: $goal');
-          for (var payment in payments) {
-            int amount = payment['amount'];
-            String date = payment['date'];
-            print('Payment - Amount: $amount, Date: $date');
-          }
+  try {
+    await ref.add(data);
+    print('Data saved successfully!');
+  } catch (e) {
+    print('Error saving data: $e');
+  }
+}
+
+void readDataFromFirebase() async {
+  CollectionReference ref = FirebaseFirestore.instance
+      .collection('users')
+      .doc(FirebaseAuth.instance.currentUser?.uid)
+      .collection('testData');
+
+  try {
+    QuerySnapshot querySnapshot = await ref.get();
+    if (querySnapshot.docs.isNotEmpty) {
+      for (QueryDocumentSnapshot documentSnapshot in querySnapshot.docs) {
+        Map<String, dynamic>? data =
+            documentSnapshot.data() as Map<String, dynamic>?;
+        // Access the fields of the document
+        String goal = data?['goal'];
+        List<dynamic> payments = data?['payments'];
+
+        // Print the data
+        print('Goal: $goal');
+        for (var payment in payments) {
+          int amount = payment['amount'];
+          String date = payment['date'];
+          print('Payment - Amount: $amount, Date: $date');
         }
-      } else {
-        print('No documents found.');
       }
-    } catch (e) {
-      print('Error reading data: $e');
+    } else {
+      print('No documents found.');
     }
+  } catch (e) {
+    print('Error reading data: $e');
   }
 }
 
